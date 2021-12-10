aadc3fda6fd03db8d8f689f8dd62b40e67245fb3
fabriciodanioj <fabricio@pop-os.localdomain>
2021-12-10 16:06:14 -0300

    fix: README

--- src/jobs/storeNewArticles.js
+++ src/jobs/storeNewArticles.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { getNewArticles } = require('../workers');
 
 module.exports = cron.schedule(
-  '0 0 * * *',
+  '0 0 9 * * *',
   async () => {
     await getNewArticles();
   },
