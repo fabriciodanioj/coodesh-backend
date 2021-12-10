e55dc7d5d180bafd639f606d98b9907da4309d2d
fabriciodanioj <fabricio@pop-os.localdomain>
2021-12-10 14:07:06 -0300

    feat: add README

--- src/validators/list.schema.js
+++ src/validators/list.schema.js
@@ -1,9 +1,9 @@
 const yup = require('../lib/yup');
 
 module.exports = yup.object().shape({
-  fields: yup.array().of(yup.string()).default(['title']),
   page: yup.number().integer().min(1),
   perPage: yup.number().integer().min(1),
   filter: yup.string(),
-  sort: yup.string().default('-createdAt'),
+  sort: yup.string().default('publishedAt'),
+  order: yup.string().default('asc'),
 });
